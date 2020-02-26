deb4ba9692c0836c057c1a5437fa166d5b8c5083
DavidBerryUK <DavidBerryUK@hotmail.co.uk>
2020-02-26 17:13:18 +0000

    format the read only version of vehicle details.

--- Client/src/components/fleetDetail/fleet-detail-component.tsx
+++ Client/src/components/fleetDetail/fleet-detail-component.tsx
@@ -1,9 +1,10 @@
-import { Button }                               from '@material-ui/core';
+import { Box }                                  from '@material-ui/core';
+import { Grid }                                 from '@material-ui/core';
+import { TextField }                            from '@material-ui/core';
 import { useMemo }                              from 'react';
 import ApiBaseItemResponseModel                 from '../../models/apiBase/ApiBaseItemResponseModel';
 import React                                    from 'react';
 import RepositoryVehicleItem                    from '../../repository/vehicle/RepositoryVehicleItem';
-import RouteConstants                           from '../../routing/RouteConstants';
 import VehicleModel                             from '../../models/vehicle/VehicleModel';
 
 interface IProperties {
@@ -12,33 +13,80 @@ interface IProperties {
 
 const FleetDetailComponent: React.FC<IProperties> = (props) => {
 
-  const [vehicleItem, setVehicleItem] = React.useState(new ApiBaseItemResponseModel<VehicleModel>());
+  const [vehicleItem, setVehicleItem] = React.useState(new VehicleModel());
 
   useMemo(() => {
     var repositoryVehicle = new RepositoryVehicleItem();
+    
     repositoryVehicle.getVehicleById(props.vehicleId)
-      .onSuccess((vehicleData: ApiBaseItemResponseModel<VehicleModel>) => {
-        console.log("Loaded Vehicle Data");
-        console.log(vehicleData);
-        setVehicleItem(vehicleData);
+      .onSuccess((vehicleData: ApiBaseItemResponseModel<VehicleModel>) => {        
+        setVehicleItem(vehicleData.entity!);
       })
   }, [props.vehicleId])
 
   return (
-    <>
-      <h1>FleetDetail:{props.vehicleId}</h1>
-      {vehicleItem.entity?.vehicleId}
-      {vehicleItem.entity?.registration}
-      {vehicleItem.entity?.colour}
-      {vehicleItem.entity?.doors}
-      {vehicleItem.entity?.make}
-      {vehicleItem.entity?.model}
-      {vehicleItem.entity?.transmission}
-      {vehicleItem.entity?.mpg}
-      {vehicleItem.entity?.mileage}
 
-      <Button variant="contained" color="primary" href={RouteConstants.FleetList}>Fleet List</Button>
-    </>
+    <Box p={4}>
+      <Grid container >
+
+        <Grid item xs={4} >
+          <TextField
+            label="Registration"
+            value={vehicleItem.registration || ""}
+            disabled />
+        </Grid>
+
+        <Grid item xs={4} >
+          <TextField
+            label="Make"
+            value={vehicleItem.make || ""}
+            disabled />
+        </Grid>
+
+        <Grid item xs={4} >
+          <TextField
+            label="Model"
+            value={vehicleItem.model || ""}
+            disabled />
+        </Grid>
+        
+        <Grid item xs={4} >
+          <TextField
+            label="Colour"
+            value={vehicleItem.colour || ""}
+            disabled />
+        </Grid>
+
+        <Grid item xs={4} >
+          <TextField
+            label="Doors"
+            value={vehicleItem.doors || ""}
+            disabled />
+        </Grid>
+        
+        <Grid item xs={4} >
+          <TextField
+            label="Transmission"
+            value={vehicleItem.transmission || ""}
+            disabled />
+        </Grid>
+
+        <Grid item xs={4} >
+          <TextField
+            label="MPG"
+            value={vehicleItem.mpg || ""}
+            disabled />
+        </Grid>
+
+        <Grid item xs={4} >
+          <TextField
+            label="Mileage"
+            value={vehicleItem.mileage || ""}
+            disabled />
+        </Grid>
+
+      </Grid>
+    </Box>
   );
 }
 
